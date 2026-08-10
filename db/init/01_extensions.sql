3609e5be1989e80ca9eb3ebe8a8380c7e913345b
bbWillsont <wedwillsont97@gmail.com>
2026-08-11 00:00:03 +1200

    Create 01_extensions.sql

---
+++ db/init/01_extensions.sql
@@ -0,0 +1,5 @@
+-- Расширения
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+CREATE EXTENSION IF NOT EXISTS "pg_trgm";
+
+COMMENT ON DATABASE CURRENT_DATABASE IS 'ElectroHistory SaaS - электромонтажные сметы и проекты';
